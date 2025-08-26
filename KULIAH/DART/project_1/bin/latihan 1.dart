6646d028c242329248efad9e35473f5df00e5124
firaz105 <firazadizia@gmail.com>
2025-08-26 11:44:04 +0900

    biodata

---
+++ KULIAH/DART/project_1/bin/latihan 1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Firaz";
+  int age = 20;
+  double height = 170;
+  num weight = 60.40;
+  dynamic Kampus = "Jati";
+
+  print("Nama : $name");
+  print("Umur : $age");
+  print("Tinggi badan : $height");
+  print("Berat badan : $weight");
+  print("Kampus : $Kampus");
+}
